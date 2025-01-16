4902e5e0cb00a756e4f9f45585ce8ac9e6131833
sammy <sammyoziegbe@gmail.com>
2025-01-16 13:40:35 +0100

    feat: setup dev environment

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import formsPlugin from '@tailwindcss/forms'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [formsPlugin, require("tailwindcss-animate")],
+}
+
