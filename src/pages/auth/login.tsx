d57d9b588008af41b3f170dec8692f29c295a7c5
sammy <sammyoziegbe@gmail.com>
2025-01-17 12:55:34 +0100

    feat: login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,19 +1,19 @@
 import { BaseButton, BaseInput } from "@/components/core"
-import { Eye, MessageCircle, User } from "iconsax-react"
+import { Eye, MessageCircle } from "iconsax-react"
 
 export const LoginPage = () => {
     return (
         <div className="flex flex-col gap-8 items-center justify-center max-w-md w-full mx-auto">
             <div className="grid gap-0.5 text-center">
-                <h1 className="text-green-primary-50 text-3xl font-semibold">You’ve been invited</h1>
-                <p className="text-grey-40 text-base font-normal">Create an account to access this application</p>
+                <h1 className="text-green-primary-50 text-3xl font-semibold">Log in to your account</h1>
+                <p className="text-grey-40 text-base font-normal">Enter your details to access this application</p>
             </div>
             <form className="flex flex-col gap-8 w-full">
-                <BaseInput label="EMAIL ADDRESS" type="text" value="Aijaysolomon68@gmail.com" disabled iconRight={<MessageCircle size="20" />} />
-                <BaseInput label="FULL NAME" type="text" placeholder="Enter full name" iconRight={<User size="20" />} />
-                <BaseInput label="PASSWORD" type="password" placeholder="Enter password" iconRight={<Eye size="20" />} />
-                <BaseInput label="CONFIRM PASSWORD" type="password" placeholder="Confirm password" iconRight={<Eye size="20" />} />
-                <BaseButton size="small" theme="primary" variant="filled" block>Accept invitation</BaseButton>
+                <div className="flex flex-col gap-7">
+                    <BaseInput label="Email Address" type="email" placeholder="Enter email" iconRight={<MessageCircle size="20" />} />
+                    <BaseInput label="Password" type="password" placeholder="Enter password" iconRight={<Eye size="20" />} />
+                </div>
+                <BaseButton size="small" theme="primary" variant="filled" block>Login</BaseButton>
             </form>
         </div>
     )
